17d2f15ecbaee6de6c0eb1ed51f5e29566aa969a
DevFaizaan <faizaan.b247@gmail.com>
2022-09-15 14:51:20 +0100

    models

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,41 +1,20 @@
 const mongoose = require('mongoose');
 
 
-const Item = mongoose.Schema({
+const Items = mongoose.Schema({
   itemName: {
     type: String,
     required: true,
   },
   quantity: {
     type: Number,
-  },
-  price: {
-    type: Number,
   }
 
-})
-const Basket = mongoose.Schema({
-  items: Item,
-  totalPrice: Item.quantity * Item.price,
 })
 
 const Order = mongoose.Schema(
   {
-    products: {
-      Basket,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    quantity: {
-      type: String,
-      required: true,
-    },
-    orderID:{
-        type: Number,
-        required: true,
-    }
+    items : Items
   },
   {
     timestamps: true,
@@ -43,4 +22,4 @@ const Order = mongoose.Schema(
 );
 
 
-module.exports = mongoose.model('customer', Customer);
+module.exports = mongoose.model('order', Order);
