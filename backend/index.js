ff3d178d4d189acdc2744ac7ce36b2408e4f6e9e
DevFaizaan <faizaan.b247@gmail.com>
2022-09-15 15:20:56 +0100

    routes

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const MongoDBConnect = require('./config/db')
 const PORT = process.env.PORT || 5000
 const dotenv = require('dotenv');
+
 require("dotenv").config({ path: "./config/.env" });
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
+
 // ----------------------
 // Database connect
 MongoDBConnect()
@@ -12,12 +15,24 @@ MongoDBConnect()
 // App initialise
 const app = express()
 
+const port = process.env.PORT;
+const customerRoutes = require('./routes/CustomerRoute');
+const driverRoutes = require('./routes/DriverRoute');
+const orderRoutes = require('./routes/OrderRoute');
+
+const connectDB = require('./config/db');
 
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const server = app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+
+app.use('/api/customer', customerRoutes);
+app.use('/api/driver', driverRoutes);
+app.use('/api/order', orderRoutes)
+const server = app.listen(port, () => console.info(`Server started on port ${port}`));
+
+(configPath === './config/config.env') && console.warn("-- WARNING: Running development environment. NOT SAFE FOR TESTING. --");
 
 module.exports = server;
